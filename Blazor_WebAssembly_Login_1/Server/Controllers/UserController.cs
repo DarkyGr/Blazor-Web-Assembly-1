41f4271ff50b50ebae3e10756f01c73c5774f540
Guillermo Riaño <guilledark17@gmail.com>
2023-06-17 11:27:59 -0600

    ✅ Adding API - UserController

---
+++ Blazor_WebAssembly_Login_1/Server/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Blazor_WebAssembly_Login_1.Shared;
+
+namespace Blazor_WebAssembly_Login_1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // Create the Login method to verify the login and validate if it is the admin by priority
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            SessionDTO sessionDTO = new SessionDTO();
+
+            if (login.Email == "admin@gmail.com" && login.Password == "admin")
+            {
+                sessionDTO.Name = "Admin";
+                sessionDTO.Email = login.Email;
+                sessionDTO.Rol = "Admin";
+            }
+            else
+            {
+                sessionDTO.Name = "Employee";
+                sessionDTO.Email = login.Email;
+                sessionDTO.Rol = "Employee";
+            }
+
+            return StatusCode(StatusCodes.Status200OK, sessionDTO);
+
+        }
+    }
+}
