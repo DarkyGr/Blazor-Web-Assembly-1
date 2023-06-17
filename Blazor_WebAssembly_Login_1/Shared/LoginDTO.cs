ef9b0fb4adc4e08fb4e84d19a350fde9d06f6829
Guillermo Riaño <guilledark17@gmail.com>
2023-06-17 11:19:23 -0600

    ✅ Adding DTO Classes

---
+++ Blazor_WebAssembly_Login_1/Shared/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_WebAssembly_Login_1.Shared
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Blazor_WebAssembly_Login_1/Shared/SessionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_WebAssembly_Login_1.Shared
+{
+    public class SessionDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Rol { get; set; }
+    }
+}
