9d46533a5c506f2c45172716f85f3c4246c812e1
Guillermo Riaño <guilledark17@gmail.com>
2023-06-18 13:19:39 -0600

    ✅ Adding Services

--- Blazor_WebAssembly_Login_1/Client/Program.cs
+++ Blazor_WebAssembly_Login_1/Client/Program.cs
@@ -2,10 +2,20 @@ using Blazor_WebAssembly_Login_1.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazor_WebAssembly_Login_1.Client.Extensions;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+// Services
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationExtension>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
